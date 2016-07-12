5511886de40697e3064af8d86e7c58da683d51b5
h82258652 <h82258652@hotmail.com>
2016-07-12 20:20:25 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/App.xaml.cs
@@ -0,0 +1,94 @@
+﻿using BingoWallpaper.Uwp.Views;
+using System;
+using UmengSDK;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace BingoWallpaper.Uwp
+{
+    internal sealed partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+            Resuming += OnResuming;
+            Suspending += OnSuspending;
+        }
+
+        protected override async void OnActivated(IActivatedEventArgs args)
+        {
+            base.OnActivated(args);
+
+            await UmengAnalytics.StartTrackAsync(Constants.UmengAppKey);
+        }
+
+        /// <summary>
+        /// 在应用程序由最终用户正常启动时进行调用。
+        /// 将在启动应用程序以打开特定文件等情况下使用。
+        /// </summary>
+        /// <param name="e">有关启动请求和过程的详细信息。</param>
+        protected override async void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // 不要在窗口已包含内容时重复应用程序初始化，
+            // 只需确保窗口处于活动状态
+            if (rootFrame == null)
+            {
+                // 创建要充当导航上下文的框架，并导航到第一页
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: 从之前挂起的应用程序加载状态
+                }
+
+                // 将框架放在当前窗口中
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // 当导航堆栈尚未还原时，导航到第一页，
+                    // 并通过将所需信息作为导航参数传入来配置
+                    // 参数
+                    rootFrame.Navigate(typeof(MainView), e.Arguments);
+                }
+                // 确保当前窗口处于活动状态
+                Window.Current.Activate();
+            }
+
+            await UmengAnalytics.StartTrackAsync(Constants.UmengAppKey);
+        }
+
+        private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        private async void OnResuming(object sender, object e)
+        {
+            await UmengAnalytics.StartTrackAsync(Constants.UmengAppKey);
+        }
+
+        private async void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            try
+            {
+                await UmengAnalytics.EndTrackAsync();
+            }
+            finally
+            {
+                deferral.Complete();
+            }
+        }
+    }
+}
---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace BingoWallpaper.Uwp
+{
+    public static class Constants
+    {
+        internal const string UmengAppKey = "56137ba2e0f55adb22005d2b";
+    }
+}
