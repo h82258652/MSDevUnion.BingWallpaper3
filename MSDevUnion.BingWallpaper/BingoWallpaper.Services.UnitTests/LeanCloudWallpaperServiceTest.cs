f3ef63e7c288a3402e5373bf955f7ed880a658b5
h82258652 <h82258652@hotmail.com>
2016-07-13 15:41:27 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Services.UnitTests/LeanCloudWallpaperServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace BingoWallpaper.Services.UnitTests
+{
+    [TestFixture]
+    public class LeanCloudWallpaperServiceTest
+    {
+        private ILeanCloudWallpaperService _service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _service = new LeanCloudWallpaperService();
+        }
+
+        [Test]
+        public void TestGetImageAsync()
+        {
+            Assert.Fail();
+        }
+    }
+}
