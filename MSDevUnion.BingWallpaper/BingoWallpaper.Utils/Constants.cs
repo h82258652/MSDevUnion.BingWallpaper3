8772ff8bd759a78e787c632b2ff4e3df49ecfd94
h82258652 <h82258652@hotmail.com>
2016-07-13 09:11:11 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Utils/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BingoWallpaper
+{
+    public static class Constants
+    {
+        public static readonly DateTimeOffset MinimumViewMonth = new DateTimeOffset(new DateTime(2015, 1, 1));
+    }
+}
