aaf4ce9c1d45bded5a7145cceb9a02558dff7a69
h82258652 <h82258652@hotmail.com>
2016-07-14 20:51:25 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Behaviors/BeginOrResumeStoryboardAction.cs
@@ -0,0 +1,30 @@
+﻿using BingoWallpaper.Uwp.Extensions;
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace BingoWallpaper.Uwp.Behaviors
+{
+    public class BeginOrResumeStoryboardAction : DependencyObject, IAction
+    {
+        public static readonly DependencyProperty StoryboardProperty = DependencyProperty.Register(nameof(Storyboard), typeof(Storyboard), typeof(BeginOrResumeStoryboardAction), new PropertyMetadata(null));
+
+        public Storyboard Storyboard
+        {
+            get
+            {
+                return (Storyboard)GetValue(StoryboardProperty);
+            }
+            set
+            {
+                SetValue(StoryboardProperty, value);
+            }
+        }
+
+        public object Execute(object sender, object parameter)
+        {
+            Storyboard?.BeginOrResume();
+            return null;
+        }
+    }
+}
--- MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Views/MainView.xaml.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Views/MainView.xaml.cs
@@ -1,20 +1,10 @@
-﻿using BingoWallpaper.Uwp.Extensions;
-using Windows.UI.Xaml.Media.Animation;
-
-namespace BingoWallpaper.Uwp.Views
+﻿namespace BingoWallpaper.Uwp.Views
 {
     public sealed partial class MainView
     {
         public MainView()
         {
             InitializeComponent();
-        }
-
-        public Storyboard RefreshBusyStoryboard => (Storyboard)Resources["RefreshBusyStoryboard"];
-
-        public void BeginOrResumeRefreshBusyStoryboard()
-        {
-            RefreshBusyStoryboard.BeginOrResume();
         }
     }
 }
