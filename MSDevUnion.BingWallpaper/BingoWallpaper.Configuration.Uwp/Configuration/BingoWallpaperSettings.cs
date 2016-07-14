636667148a37d3e921f1c5fac33a8254d9a87a82
h82258652 <h82258652@hotmail.com>
2016-07-14 17:28:43 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Configuration.Uwp/Configuration/BingoWallpaperSettings.cs
@@ -0,0 +1,38 @@
+﻿using BingoWallpaper.Services;
+using System;
+using System.Globalization;
+using System.Linq;
+using Windows.Storage;
+
+namespace BingoWallpaper.Configuration
+{
+    public class BingoWallpaperSettings : AppSettingsBase, IBingoWallpaperSettings
+    {
+        private readonly ILeanCloudWallpaperService _leanCloudWallpaperService;
+
+        public BingoWallpaperSettings(ILeanCloudWallpaperService leanCloudWallpaperService)
+        {
+            _leanCloudWallpaperService = leanCloudWallpaperService;
+        }
+
+        public string SelectedArea
+        {
+            get
+            {
+                return Get(nameof(SelectedArea), ApplicationDataLocality.Roaming, () =>
+                {
+                    var currentCulture = CultureInfo.CurrentCulture.Name;
+                    if (_leanCloudWallpaperService.GetSupportedAreas().Contains(currentCulture, StringComparer.OrdinalIgnoreCase))
+                    {
+                        return currentCulture;
+                    }
+                    return "en-US";
+                });
+            }
+            set
+            {
+                Set(nameof(SelectedArea), value, ApplicationDataLocality.Roaming);
+            }
+        }
+    }
+}
