281a2dfbe129ea78c9f153ac4b95dead43066709
h82258652 <h82258652@hotmail.com>
2016-07-14 10:42:09 +0800

    update

--- MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/ViewModels/MainViewModel.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/ViewModels/MainViewModel.cs
@@ -1,14 +1,22 @@
 ﻿using BingoWallpaper.Models.LeanCloud;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Views;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BingoWallpaper.Uwp.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
         private readonly INavigationService _navigationService;
+
+        private bool _isBusy;
+
+        private RelayCommand _refreshCommand;
+
+        private WallpaperCollection _selectedWallpaperCollection;
 
         public MainViewModel(INavigationService navigationService)
         {
@@ -22,6 +30,58 @@ namespace BingoWallpaper.Uwp.ViewModels
                 date = date.AddMonths(1);
             }
             WallpaperCollections = wallpaperCollections;
+        }
+
+        public bool IsBusy
+        {
+            get
+            {
+                return _isBusy;
+            }
+            private set
+            {
+                Set(ref _isBusy, value);
+            }
+        }
+
+        public RelayCommand RefreshCommand
+        {
+            get
+            {
+                _refreshCommand = _refreshCommand ?? new RelayCommand(() =>
+                {
+                    IsBusy = true;
+                    try
+                    {
+                        throw new NotImplementedException();
+                    }
+                    finally
+                    {
+                        IsBusy = false;
+                    }
+                });
+                return _refreshCommand;
+            }
+        }
+
+        public WallpaperCollection SelectedWallpaperCollection
+        {
+            get
+            {
+                if (_selectedWallpaperCollection == null)
+                {
+                    SelectedWallpaperCollection = WallpaperCollections.LastOrDefault();
+                }
+                return _selectedWallpaperCollection;
+            }
+            set
+            {
+                Set(ref _selectedWallpaperCollection, value);
+                if (_selectedWallpaperCollection != value)
+                {
+                    RefreshCommand.Execute(null);
+                }
+            }
         }
 
         public IReadOnlyList<WallpaperCollection> WallpaperCollections
