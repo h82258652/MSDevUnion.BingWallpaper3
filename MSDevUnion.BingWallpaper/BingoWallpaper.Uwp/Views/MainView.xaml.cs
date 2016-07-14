7f54c2b7aac0ee4f22749219dd12562fae220861
h82258652 <h82258652@hotmail.com>
2016-07-14 13:57:13 +0800

    update

--- MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Views/MainView.xaml.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Views/MainView.xaml.cs
@@ -1,10 +1,20 @@
-﻿namespace BingoWallpaper.Uwp.Views
+﻿using BingoWallpaper.Uwp.Extensions;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace BingoWallpaper.Uwp.Views
 {
     public sealed partial class MainView
     {
         public MainView()
         {
             InitializeComponent();
+        }
+
+        public Storyboard RefreshBusyStoryboard => (Storyboard)Resources["RefreshBusyStoryboard"];
+
+        public void BeginOrResumeRefreshBusyStoryboard()
+        {
+            RefreshBusyStoryboard.BeginOrResume();
         }
     }
 }
