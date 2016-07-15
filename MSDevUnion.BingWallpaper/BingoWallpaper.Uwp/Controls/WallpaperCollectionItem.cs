06eedd70cc202b78d657a61927c82ac29f53fe5c
h82258652 <h82258652@hotmail.com>
2016-07-15 13:04:48 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Controls/WallpaperCollectionItem.cs
@@ -0,0 +1,28 @@
+﻿using BingoWallpaper.Models.LeanCloud;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace BingoWallpaper.Uwp.Controls
+{
+    public sealed class WallpaperCollectionItem : Control
+    {
+        public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register(nameof(ItemsSource), typeof(WallpaperCollection), typeof(WallpaperCollectionItem), new PropertyMetadata(null));
+
+        public WallpaperCollectionItem()
+        {
+            DefaultStyleKey = typeof(WallpaperCollectionItem);
+        }
+
+        public WallpaperCollection ItemsSource
+        {
+            get
+            {
+                return (WallpaperCollection)GetValue(ItemsSourceProperty);
+            }
+            set
+            {
+                SetValue(ItemsSourceProperty, value);
+            }
+        }
+    }
+}
