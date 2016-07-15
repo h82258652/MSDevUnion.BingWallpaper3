5852ba7e8026cc8da8582d369182dd5a12c4a9a3
h82258652 <h82258652@hotmail.com>
2016-07-15 15:37:28 +0800

    update

--- MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Controls/WallpaperCollectionItem.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Controls/WallpaperCollectionItem.cs
@@ -6,11 +6,25 @@ namespace BingoWallpaper.Uwp.Controls
 {
     public sealed class WallpaperCollectionItem : Control
     {
+        public static readonly DependencyProperty IsBusyProperty = DependencyProperty.Register(nameof(IsBusy), typeof(bool), typeof(WallpaperCollectionItem), new PropertyMetadata(false));
+
         public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register(nameof(ItemsSource), typeof(WallpaperCollection), typeof(WallpaperCollectionItem), new PropertyMetadata(null));
 
         public WallpaperCollectionItem()
         {
             DefaultStyleKey = typeof(WallpaperCollectionItem);
+        }
+
+        public bool IsBusy
+        {
+            get
+            {
+                return (bool)GetValue(IsBusyProperty);
+            }
+            set
+            {
+                SetValue(IsBusyProperty, value);
+            }
         }
 
         public WallpaperCollection ItemsSource
