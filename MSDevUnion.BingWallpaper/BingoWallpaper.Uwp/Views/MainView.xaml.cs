843baaf7a8130dd6b6dd8af89311d542ce75a841
h82258652 <h82258652@hotmail.com>
2016-07-19 16:32:19 +0800

    update

--- MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Views/MainView.xaml.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Views/MainView.xaml.cs
@@ -1,10 +1,21 @@
-﻿namespace BingoWallpaper.Uwp.Views
+﻿using BingoWallpaper.Uwp.Controls;
+using System;
+using Windows.UI.Xaml;
+
+namespace BingoWallpaper.Uwp.Views
 {
     public sealed partial class MainView
     {
         public MainView()
         {
             InitializeComponent();
+        }
+
+        private void FirstDoubleSizePanel_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var panel = (FirstDoubleSizePanel)sender;
+            var width = e.NewSize.Width;
+            panel.MaximumRowsOrColumns = Math.Max((int)(width / 320), 2);
         }
     }
 }
