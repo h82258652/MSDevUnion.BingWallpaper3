715f300728b1383375dc49e37e20bfed374b616d
h82258652 <h82258652@hotmail.com>
2016-07-19 22:14:25 +0800

    update

--- MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/ViewModels/DetailViewModel.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/ViewModels/DetailViewModel.cs
@@ -1,8 +1,31 @@
-﻿using GalaSoft.MvvmLight;
+﻿using BingoWallpaper.Models.LeanCloud;
+using GalaSoft.MvvmLight;
 
 namespace BingoWallpaper.Uwp.ViewModels
 {
-    public class DetailViewModel : ViewModelBase
+    public class DetailViewModel : ViewModelBase, INavigable
     {
+        private Wallpaper _wallpaper;
+
+        public Wallpaper Wallpaper
+        {
+            get
+            {
+                return _wallpaper;
+            }
+            private set
+            {
+                Set(ref _wallpaper, value);
+            }
+        }
+
+        public void Activate(object parameter)
+        {
+            Wallpaper = (Wallpaper)parameter;
+        }
+
+        public void Deactivate(object parameter)
+        {
+        }
     }
 }
