ea36f00b49dd6ce2b6df9cc01323936739130c3e
h82258652 <h82258652@hotmail.com>
2016-07-21 10:43:36 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Views/ViewBase.cs
@@ -0,0 +1,72 @@
+﻿using Windows.Foundation.Metadata;
+using Windows.Phone.UI.Input;
+using Windows.UI.Core;
+using Windows.UI.Input;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
+
+namespace BingoWallpaper.Uwp.Views
+{
+    public abstract class ViewBase : BindablePage
+    {
+        private readonly SystemNavigationManager _systemNavigationManager;
+
+        protected ViewBase()
+        {
+            _systemNavigationManager = SystemNavigationManager.GetForCurrentView();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            _systemNavigationManager.AppViewBackButtonVisibility = Frame.CanGoBack ? AppViewBackButtonVisibility.Visible : AppViewBackButtonVisibility.Collapsed;
+            _systemNavigationManager.BackRequested -= SystemNavigationManager_BackRequested;
+
+            if (ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons"))
+            {
+                HardwareButtons.BackPressed -= HardwareButtons_BackPressed;
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            _systemNavigationManager.AppViewBackButtonVisibility = Frame.CanGoBack ? AppViewBackButtonVisibility.Visible : AppViewBackButtonVisibility.Collapsed;
+            _systemNavigationManager.BackRequested += SystemNavigationManager_BackRequested;
+
+            if (ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons"))
+            {
+                HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+            }
+        }
+
+        protected override void OnPointerReleased(PointerRoutedEventArgs e)
+        {
+            base.OnPointerReleased(e);
+
+            var properties = e.GetCurrentPoint(null).Properties;
+            switch (properties.PointerUpdateKind)
+            {
+                case PointerUpdateKind.XButton1Released:
+                    // TODO
+                    break;
+
+                case PointerUpdateKind.XButton2Released:
+                    // TODO
+                    break;
+            }
+        }
+
+        private void HardwareButtons_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            // TODO
+        }
+
+        private void SystemNavigationManager_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            // TODO
+        }
+    }
+}
