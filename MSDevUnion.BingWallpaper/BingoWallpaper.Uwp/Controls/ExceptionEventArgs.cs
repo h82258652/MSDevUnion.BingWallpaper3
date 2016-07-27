0f088c3c71ca87389b494f56b5df4eb351c636c8
h82258652 <h82258652@hotmail.com>
2016-07-27 17:56:43 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Controls/ExceptionEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BingoWallpaper.Uwp.Controls
+{
+    public class ExceptionEventArgs : EventArgs
+    {
+        internal ExceptionEventArgs(string errorMessage)
+        {
+            ErrorMessage = errorMessage;
+        }
+
+        public string ErrorMessage
+        {
+            get;
+        }
+    }
+}
---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Controls/ImageFailedEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BingoWallpaper.Uwp.Controls
+{
+    public class ImageFailedEventArgs : ExceptionEventArgs
+    {
+        internal ImageFailedEventArgs(string url, string errorMessage) : base(errorMessage)
+        {
+            if (url == null)
+            {
+                throw new ArgumentNullException(nameof(url));
+            }
+
+            Url = url;
+        }
+
+        public string Url
+        {
+            get;
+        }
+    }
+}
