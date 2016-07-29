b987fe67fba42fa271bd82e0eb2b970569090bdc
h82258652 <h82258652@hotmail.com>
2016-07-29 09:06:28 +0800

    update

--- MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/ViewModels/SettingViewModel.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/ViewModels/SettingViewModel.cs
@@ -1,16 +1,41 @@
 ﻿using BingoWallpaper.Configuration;
 using BingoWallpaper.Models;
+using BingoWallpaper.Services;
 using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BingoWallpaper.Uwp.ViewModels
 {
     public class SettingViewModel : ViewModelBase
     {
+        private readonly ILeanCloudWallpaperService _leanCloudWallpaperService;
+
         private readonly IBingoWallpaperSettings _settings;
 
-        public SettingViewModel(IBingoWallpaperSettings settings)
+        public SettingViewModel(ILeanCloudWallpaperService leanCloudWallpaperService, IBingoWallpaperSettings settings)
         {
+            _leanCloudWallpaperService = leanCloudWallpaperService;
             _settings = settings;
+        }
+
+        public IReadOnlyList<SaveLocation> SaveLocations
+        {
+            get;
+        } = Enum.GetValues(typeof(SaveLocation)).Cast<SaveLocation>().ToList();
+
+        public SaveLocation SelectedSaveLocation
+        {
+            get
+            {
+                return _settings.SelectedSaveLocation;
+            }
+            set
+            {
+                _settings.SelectedSaveLocation = value;
+                RaisePropertyChanged();
+            }
         }
 
         public WallpaperSize SelectedWallpaperSize
@@ -25,5 +50,7 @@ namespace BingoWallpaper.Uwp.ViewModels
                 RaisePropertyChanged();
             }
         }
+
+        public IReadOnlyList<WallpaperSize> WallpaperSizes => _leanCloudWallpaperService.GetSupportedWallpaperSizes();
     }
 }
