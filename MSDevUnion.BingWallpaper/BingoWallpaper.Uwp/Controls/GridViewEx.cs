2205f4524e0b50e2b1d6f2800582b960460abd94
h82258652 <h82258652@hotmail.com>
2016-08-04 16:13:20 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Controls/GridViewEx.cs
@@ -0,0 +1,39 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace BingoWallpaper.Uwp.Controls
+{
+    public class GridViewEx : GridView
+    {
+        public event PointerEventHandler ItemPointerEntered;
+
+        public event PointerEventHandler ItemPointerExited;
+
+        protected override void ClearContainerForItemOverride(DependencyObject element, object item)
+        {
+            var gridViewItem = (GridViewItem)element;
+            gridViewItem.PointerEntered -= GridViewItem_PointerEntered;
+            gridViewItem.PointerExited -= GridViewItem_PointerExited;
+            base.ClearContainerForItemOverride(element, item);
+        }
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            var gridViewItem = (GridViewItem)base.GetContainerForItemOverride();
+            gridViewItem.PointerEntered += GridViewItem_PointerEntered;
+            gridViewItem.PointerExited += GridViewItem_PointerExited;
+            return gridViewItem;
+        }
+
+        private void GridViewItem_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            ItemPointerEntered?.Invoke(sender, e);
+        }
+
+        private void GridViewItem_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            ItemPointerExited?.Invoke(sender, e);
+        }
+    }
+}
--- MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Views/MainView.xaml.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Views/MainView.xaml.cs
@@ -1,7 +1,13 @@
 ﻿using BingoWallpaper.Uwp.Controls;
 using System;
+using Windows.Devices.Input;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
+using WinRTXamlToolkit.AwaitableUI;
+using WinRTXamlToolkit.Controls.Extensions;
 
 namespace BingoWallpaper.Uwp.Views
 {
@@ -18,6 +24,113 @@ namespace BingoWallpaper.Uwp.Views
             var panel = (FirstDoubleSizePanel)sender;
             var width = e.NewSize.Width;
             panel.MaximumRowsOrColumns = Math.Max((int)(width / 320), 2);
+        }
+
+        private void GridViewEx_ItemPointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            if (e.Pointer.PointerDeviceType == PointerDeviceType.Mouse)
+            {
+                var element = (UIElement)sender;
+                Canvas.SetZIndex(element, int.MaxValue);
+                var textBlock = element.GetFirstDescendantOfType<TextBlock>();
+                var border = element.GetFirstAncestorOfType<Border>();
+                var storyboard = new Storyboard();
+                {
+                    var animation = new DoubleAnimation
+                    {
+                        To = 1.05,
+                        Duration = TimeSpan.FromSeconds(0.1)
+                    };
+                    Storyboard.SetTarget(animation, element);
+                    Storyboard.SetTargetProperty(animation, "(UIElement.RenderTransform).(ScaleTransform.ScaleX)");
+                    storyboard.Children.Add(animation);
+                }
+                {
+                    var animation = new DoubleAnimation
+                    {
+                        To = 1.05,
+                        Duration = TimeSpan.FromSeconds(0.1)
+                    };
+                    Storyboard.SetTarget(animation, element);
+                    Storyboard.SetTargetProperty(animation, "(UIElement.RenderTransform).(ScaleTransform.ScaleY)");
+                    storyboard.Children.Add(animation);
+                }
+                {
+                    var animation = new DoubleAnimation
+                    {
+                        To = border.ActualWidth,
+                        Duration = TimeSpan.FromSeconds(0.1)
+                    };
+                    Storyboard.SetTarget(animation, textBlock);
+                    Storyboard.SetTargetProperty(animation, "(UIElement.RenderTransform).(TranslateTransform.Y)");
+                    storyboard.Children.Add(animation);
+                }
+                {
+                    var animation = new DoubleAnimation
+                    {
+                        To = 0,
+                        Duration = TimeSpan.FromSeconds(0.1)
+                    };
+                    Storyboard.SetTarget(animation, border);
+                    Storyboard.SetTargetProperty(animation, "Opacity");
+                    storyboard.Children.Add(animation);
+                }
+                storyboard.Begin();
+            }
+        }
+
+        private async void GridViewEx_ItemPointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            if (e.Pointer.PointerDeviceType == PointerDeviceType.Mouse)
+            {
+                var element = (UIElement)sender;
+                Canvas.SetZIndex(element, 1);
+                var textBlock = element.GetFirstDescendantOfType<TextBlock>();
+                var border = element.GetFirstAncestorOfType<Border>();
+                var storyboard = new Storyboard();
+                {
+                    var animation = new DoubleAnimation
+                    {
+                        To = 1,
+                        Duration = TimeSpan.FromSeconds(0.1)
+                    };
+                    Storyboard.SetTarget(animation, element);
+                    Storyboard.SetTargetProperty(animation, "(UIElement.RenderTransform).(ScaleTransform.ScaleX)");
+                    storyboard.Children.Add(animation);
+                }
+                {
+                    var animation = new DoubleAnimation
+                    {
+                        To = 1,
+                        Duration = TimeSpan.FromSeconds(0.1)
+                    };
+                    Storyboard.SetTarget(animation, element);
+                    Storyboard.SetTargetProperty(animation, "(UIElement.RenderTransform).(ScaleTransform.ScaleY)");
+                    storyboard.Children.Add(animation);
+                }
+                {
+                    var animation = new DoubleAnimation
+                    {
+                        To = 0,
+                        Duration = TimeSpan.FromSeconds(0.1)
+                    };
+                    Storyboard.SetTarget(animation, textBlock);
+                    Storyboard.SetTargetProperty(animation, "(UIElement.RenderTransform).(TranslateTransform.Y)");
+                    storyboard.Children.Add(animation);
+                }
+                {
+                    var animation = new DoubleAnimation
+                    {
+                        To = 1,
+                        Duration = TimeSpan.FromSeconds(0.1)
+                    };
+                    Storyboard.SetTarget(animation, border);
+                    Storyboard.SetTargetProperty(animation, "Opacity");
+                    storyboard.Children.Add(animation);
+                }
+                await storyboard.BeginAsync();
+                Canvas.SetZIndex(element, 0);
+            }
         }
     }
 }
