5b841430f25f55590ebe03782f7de699ab7318eb
h82258652 <h82258652@hotmail.com>
2016-08-04 09:56:27 +0800

    update

--- MSDevUnion.BingWallpaper/BingoWallpaper.Services.UnitTests/BingWallpaperServicceTest.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Services.UnitTests/BingWallpaperServicceTest.cs
@@ -1,9 +1,30 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace BingoWallpaper.Services.UnitTests
 {
     [TestFixture]
     public class BingWallpaperServicceTest
     {
+        private IBingWallpaperService _bingWallpaperService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _bingWallpaperService = new BingWallpaperService();
+        }
+
+        [Test]
+        public void TestGetAsync()
+        {
+            Assert.ThrowsAsync<ArgumentOutOfRangeException>(async () =>
+            {
+                await _bingWallpaperService.GetAsync(0, 0, "zh-CN");
+            });
+            Assert.ThrowsAsync<ArgumentNullException>(async () =>
+            {
+                await _bingWallpaperService.GetAsync(0, 1, null);
+            });
+        }
     }
 }
