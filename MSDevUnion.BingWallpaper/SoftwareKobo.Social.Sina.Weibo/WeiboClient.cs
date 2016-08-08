a0eb4d6d97c5ee8784c87e35d15bcd3ee6d46ce9
h82258652 <h82258652@hotmail.com>
2016-08-08 17:45:57 +0800

    update

--- MSDevUnion.BingWallpaper/SoftwareKobo.Social.Sina.Weibo/WeiboClient.cs
+++ MSDevUnion.BingWallpaper/SoftwareKobo.Social.Sina.Weibo/WeiboClient.cs
@@ -1,37 +1,86 @@
-﻿using System;
+﻿using SoftwareKobo.Social.Sina.Weibo.Extensions;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Globalization;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources.Core;
+using Windows.Foundation;
+using Windows.Security.Authentication.Web;
 
 namespace SoftwareKobo.Social.Sina.Weibo
 {
     public class WeiboClient
     {
-        private WeiboClient()
+        public async void ShareImageAsync(byte[] image, string text)
         {
         }
 
-        public static async Task<WeiboClient> CreateAsync()
+        public async Task AuthorizeAsync(string appKey, string appSecret, string redirectUri)
         {
-            WeiboClient client = new WeiboClient();
-
-            return client;
-        }
-
-        public async Task ShareImageAsync(byte[] image, string text)
-        {
-            if (image == null)
+            if (appKey == null)
             {
-                throw new ArgumentNullException(nameof(image));
+                throw new ArgumentNullException(nameof(appKey));
             }
-            if (text == null)
+            if (appSecret == null)
             {
-                throw new ArgumentNullException(nameof(text));
+                throw new ArgumentNullException(nameof(appSecret));
             }
-            if (string.IsNullOrWhiteSpace(text))
+            if (redirectUri == null)
             {
-                throw new ArgumentException($"{nameof(text)} 不能为空白字符。", nameof(text));
+                throw new ArgumentNullException(nameof(redirectUri));
+            }
+
+            var query = new Dictionary<string, string>()
+            {
+                ["client_id"] = appKey,
+                ["redirect_uri"] = redirectUri
+            };
+            var qualifiers = ResourceContext.GetForCurrentView().QualifierValues;
+            if (qualifiers.ContainsKey("DeviceFamily") && qualifiers["DeviceFamily"] == "Mobile")
+            {
+                query["display"] = "mobile";
+            }
+            if (CultureInfo.CurrentUICulture.Name.StartsWith("zh", StringComparison.OrdinalIgnoreCase) == false)
+            {
+                query["language"] = "en";
+            }
+            var requestUri = new Uri("https://api.weibo.com/oauth2/authorize?" + query.ToUriQuery(), UriKind.Absolute);
+            var result = await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None, requestUri,
+                new Uri(redirectUri));
+            switch (result.ResponseStatus)
+            {
+                case WebAuthenticationStatus.Success:
+                    var responseUrl = result.ResponseData;
+                    var responseUri = new Uri(responseUrl);
+                    var decoder = new WwwFormUrlDecoder(responseUri.Query);
+                    var authorizeCode = decoder.GetFirstValueByName("code");
+
+                    var parameters = new Dictionary<string, string>()
+                    {
+                        ["client_id"] = appKey,
+                        ["client_secret"] = appSecret,
+                        ["grant_type"] = "authorization_code",
+                        ["code"] = authorizeCode,
+                        ["redirect_uri"] = redirectUri
+                    };
+                    var content = new FormUrlEncodedContent(parameters);
+                    using (var client = new HttpClient())
+                    {
+                        var response = await client.PostAsync("https://api.weibo.com/oauth2/access_token", content);
+                        var json = await response.Content.ReadAsStringAsync();
+                        var accessToken = JsonConvert.
+                    }
+                    break;
+
+                case WebAuthenticationStatus.UserCancel:
+                    break;
+
+                case WebAuthenticationStatus.ErrorHttp:
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
     }
