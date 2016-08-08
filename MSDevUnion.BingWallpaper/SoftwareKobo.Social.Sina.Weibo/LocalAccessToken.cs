02f295962902bd0d4bbbc4f98949ef9fed7164fd
h82258652 <h82258652@hotmail.com>
2016-08-08 17:31:16 +0800

    update

---
+++ MSDevUnion.BingWallpaper/SoftwareKobo.Social.Sina.Weibo/LocalAccessToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareKobo.Social.Sina.Weibo
+{
+    internal static class LocalAccessToken
+    {
+        internal static string Token
+        {
+            get
+            {
+            }
+            set
+            {
+            }
+        }
+    }
+}
---
+++ MSDevUnion.BingWallpaper/SoftwareKobo.Social.Sina.Weibo/WeiboClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareKobo.Social.Sina.Weibo
+{
+    public class WeiboClient
+    {
+        private WeiboClient()
+        {
+        }
+
+        public static async Task<WeiboClient> CreateAsync()
+        {
+            WeiboClient client = new WeiboClient();
+
+            return client;
+        }
+
+        public async Task ShareImageAsync(byte[] image, string text)
+        {
+            if (image == null)
+            {
+                throw new ArgumentNullException(nameof(image));
+            }
+            if (text == null)
+            {
+                throw new ArgumentNullException(nameof(text));
+            }
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                throw new ArgumentException($"{nameof(text)} 不能为空白字符。", nameof(text));
+            }
+        }
+    }
+}
