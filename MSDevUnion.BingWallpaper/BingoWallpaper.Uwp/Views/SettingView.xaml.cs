4f91b67059e58ab8b69d19d8c13e0754ffa4f13e
h82258652 <h82258652@hotmail.com>
2016-08-10 18:04:25 +0800

    update

--- MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Views/SettingView.xaml.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Views/SettingView.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace BingoWallpaper.Uwp.Views
+﻿using System;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace BingoWallpaper.Uwp.Views
 {
     public sealed partial class SettingView
     {
