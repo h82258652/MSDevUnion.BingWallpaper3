4588df392b58ccbaed041062d475779624603404
h82258652 <h82258652@hotmail.com>
2016-08-11 17:42:37 +0800

    update

--- MSDevUnion.BingWallpaper/SoftwareKobo.Social.SinaWeibo.Core/WeiboClientBase.cs
+++ MSDevUnion.BingWallpaper/SoftwareKobo.Social.SinaWeibo.Core/WeiboClientBase.cs
@@ -1,6 +1,44 @@
-﻿namespace SoftwareKobo.Social.SinaWeibo
+﻿using System;
+
+namespace SoftwareKobo.Social.SinaWeibo
 {
     public abstract class WeiboClientBase
     {
+        protected WeiboClientBase(string appKey, string appSecret, string redirectUri, string scope = null)
+        {
+            if (appKey == null)
+            {
+                throw new ArgumentNullException(nameof(appKey));
+            }
+            if (appSecret == null)
+            {
+                throw new ArgumentNullException(nameof(appSecret));
+            }
+            if (redirectUri == null)
+            {
+                throw new ArgumentNullException(nameof(redirectUri));
+            }
+
+            AppKey = appKey;
+            // TODO
+        }
+
+        protected string Scope
+        {
+            get;
+            private set;
+        }
+
+        public string Uid
+        {
+            get;
+            protected set;
+        }
+
+        protected string AppKey
+        {
+            get;
+            private set;
+        }
     }
 }
