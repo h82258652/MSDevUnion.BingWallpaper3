c640eb6835a0c0cad7e1078edebb3829961db689
h82258652 <h82258652@hotmail.com>
2016-08-12 13:21:03 +0800

    update

--- MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/LocalizedStrings.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/LocalizedStrings.cs
@@ -6,10 +6,15 @@ namespace BingoWallpaper.Uwp
     {
         private const string ConstantsReswName = "Constants";
 
+        internal static string ChooseEveryTime => ResourceLoader.GetForCurrentView(ConstantsReswName).GetString("ChooseEveryTime");
+
+        internal static string PictureLibrary => ResourceLoader.GetForCurrentView(ConstantsReswName).GetString("PictureLibrary");
+
+        internal static string SavedPictures => ResourceLoader.GetForCurrentView(ConstantsReswName).GetString("SavedPictures");
+
         internal static string SaveSuccess => ResourceLoader.GetForCurrentView(ConstantsReswName).GetString("SaveSuccess");
 
         internal static string SetFailed => ResourceLoader.GetForCurrentView(ConstantsReswName).GetString("SetFailed");
-
         internal static string SetSuccess => ResourceLoader.GetForCurrentView(ConstantsReswName).GetString("SetSuccess");
 
         internal static string ShareSuccess => ResourceLoader.GetForCurrentView(ConstantsReswName).GetString("ShareSuccess");
