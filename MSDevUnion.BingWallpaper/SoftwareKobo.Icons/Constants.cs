3b464513fffa46065557f754ca36caf0a4d9419e
h82258652 <h82258652@hotmail.com>
2016-08-15 14:50:06 +0800

    update

---
+++ MSDevUnion.BingWallpaper/SoftwareKobo.Icons/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace SoftwareKobo.Icons
+{
+    internal static class Constants
+    {
+        internal const string DashiconsFamilyName = "ms-appx:///SoftwareKobo.Icons/Assets/Fonts/dashicons.ttf#Dashicons";
+    }
+}
