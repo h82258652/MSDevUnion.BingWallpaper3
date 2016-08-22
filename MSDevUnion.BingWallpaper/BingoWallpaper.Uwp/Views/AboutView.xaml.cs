cc7d95fdcde08c847fdebd0625b9d36a7a3ec0fe
h82258652 <h82258652@hotmail.com>
2016-08-22 08:11:19 +0800

    update

--- MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Views/AboutView.xaml.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Uwp/Views/AboutView.xaml.cs
@@ -1,10 +1,21 @@
-﻿namespace BingoWallpaper.Uwp.Views
+﻿using Windows.UI.Popups;
+using Windows.UI.Xaml;
+
+namespace BingoWallpaper.Uwp.Views
 {
     public sealed partial class AboutView
     {
         public AboutView()
         {
             InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var d = new VisualTransition().GeneratedDuration;
+            new MessageDialog(d.ToString()).ShowAsync();
+
+            CheckBox.IsChecked = true;
         }
     }
 }
