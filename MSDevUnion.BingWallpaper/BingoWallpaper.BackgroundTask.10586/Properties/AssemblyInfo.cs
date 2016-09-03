a3ac11f3fc013ee0e69f298eb60309fe6ca266d7
h82258652 <h82258652@hotmail.com>
2016-09-03 18:59:29 +0800

    修改AssemblyInfo的名字符合csproj的名字

--- MSDevUnion.BingWallpaper/BingoWallpaper.BackgroundTask.10586/Properties/AssemblyInfo.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.BackgroundTask.10586/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BingoWallpaper.BackgroundTask")]
+[assembly: AssemblyTitle("BingoWallpaper.BackgroundTask.10586")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BingoWallpaper.BackgroundTask")]
+[assembly: AssemblyProduct("BingoWallpaper.BackgroundTask.10586")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- MSDevUnion.BingWallpaper/BingoWallpaper.Configuration.Uwp.10586/Properties/AssemblyInfo.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Configuration.Uwp.10586/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BingoWallpaper.Configuration.Uwp")]
+[assembly: AssemblyTitle("BingoWallpaper.Configuration.Uwp.10586")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BingoWallpaper.Configuration.Uwp")]
+[assembly: AssemblyProduct("BingoWallpaper.Configuration.Uwp.10586")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
